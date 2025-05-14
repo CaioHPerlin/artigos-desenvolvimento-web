35ca4dc70b26495bd5c97d7581a303e8d5ab2d6f
CaioHPerlin <caiohperlin@gmail.com>
2025-05-13 21:00:02 -0400

    feat: implement new article attribute 'autor'

--- controllers/artigo.controller.js
+++ controllers/artigo.controller.js
@@ -2,34 +2,38 @@ const artigos = [
   {
     titulo: "JavaScript",
     descricao: "Uma introdução ao mundo do JavaScript e suas aplicações.",
+    autor: 'Robert Robert',
     paginas: 10,
   },
   {
     titulo: "Python",
     descricao: "Explorando como Python domina o mundo da ciência de dados.",
+    autor: 'Funny Valentine',
     paginas: 8,
   },
   {
     titulo: "Rust",
     descricao: "Por que Rust é a escolha para sistemas de alta performance.",
+    autor: 'John John',
     paginas: 12,
   },
   {
     titulo: "Go",
     descricao: "Como Go está transformando o desenvolvimento backend.",
+    autor: 'Daniel Daniel',
     paginas: 7,
   },
 ];
 
 const artigoController = {
   async create(req, res) {
-    const { titulo, descricao, paginas } = req.body;
+    const { titulo, descricao, autor, paginas } = req.body;
 
-    if (!titulo || !descricao || !paginas) {
+    if (!titulo || !descricao || !autor || !paginas) {
       return res.status(400).send("Todos os campos são obrigatórios.");
     }
 
-    const novoArtigo = { titulo, descricao, paginas };
+    const novoArtigo = { titulo, autor, descricao, paginas };
     artigos.push(novoArtigo);
     res.status(201).json(novoArtigo);
   },
@@ -50,14 +54,21 @@ const artigoController = {
   },
 
   async updateByTitle(req, res) {
-    const { titulo } = req.params;
-    const artigoEncontrado = artigos.find((artigo) => artigo.titulo === titulo);
+    const { titulo: tituloParam } = req.params;
+    const index = artigos.findIndex((artigo) => artigo.titulo === tituloParam);
 
-    if (!artigoEncontrado) {
-      return res.status(404).send(`Artigo "${titulo}" não encontrado`);
+    if (index === -1) {
+      return res.status(404).send(`Artigo "${tituloParam}" não encontrado`);
     }
 
-    return res.status(200).json(artigoEncontrado);
+    const { titulo, descricao, autor, paginas } = req.body;
+    if (!titulo || !descricao || !autor || !paginas) {
+      return res.status(400).send("Todos os campos são obrigatórios.");
+    }
+
+    const novoArtigo = { titulo, autor, descricao, paginas };
+    artigos[index] = novoArtigo
+    return res.status(200).json(novoArtigo);
   },
 
   async deleleteByTitle(req, res) {
